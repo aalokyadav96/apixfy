00dcb379737b365ce9966b400e4f768d958dad7c
Aalok Yadav <35891906+aalokyadav96@users.noreply.github.com>
2023-12-04 11:09:23 +0530

    Add files via upload

--- main.go
+++ main.go
@@ -18,22 +18,31 @@ func main() {
 }
 
 func Index(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
 	fmt.Fprintf(w,"V1")
 }
 
 func API(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
 	fmt.Fprintf(w,"V1")
 }
 
 func Test(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
 	fmt.Fprintf(w,"V1")
 }
 
 
 func Status(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
 	fmt.Println(r.URL.Path)
 	FileName := r.URL.Path[len("/v1/gfycats/fetch/status/"):]
 	var jsonStr = "{\"task\":\"complete\",\"gfyname\":\"" + FileName + "\"}"
 	fmt.Println(jsonStr)
+	w.Header().Set("Content-Type", "application/json")
 	fmt.Fprintf(w, jsonStr)
+}
+
+func enableCors(w *http.ResponseWriter) {
+(*w).Header().Set("Access-Control-Allow-Origin", "https://xfy.onrender.com/")
 }
